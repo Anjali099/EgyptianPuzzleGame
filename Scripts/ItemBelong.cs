243d06a230a811721da046cd895c3b46d04f8234
Anjali099 <45853974+Anjali099@users.noreply.github.com>
2021-08-16 16:41:10 +0100

    Add files via upload

---
+++ Scripts/ItemBelong.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ItemBelong : MonoBehaviour
+{
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+ 
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // if an object collides with the object attached to this script 
+        //then find out if it is called cube and then make it a child of this cube and disable any not needed compoent of that object
+       // cube = GameObject.Find("cube");
+
+    }
+
+    void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.tag == "i1")
+        {
+            Debug.Log("right one");
+            collision.gameObject.GetComponent<pickObjects>().rb.isKinematic = true;
+            collision.gameObject.GetComponentInChildren<pickObjects>().rb = null;
+            GameObject.Find("Cube").GetComponent<pickObjects>().enabled = false;
+           
+        }
+        else {
+            Debug.Log("not the right item");
+        }
+
+      
+    }
+    // how to lock the UI so the cross does not move
+
+
+ 
+       
+
+
+    }
+//https://docs.unity3d.com/ScriptReference/GameObject.Find.html
+//https://answers.unity.com/questions/632792/how-to-make-an-object-a-child-of-another-object-sc.html
+//https://docs.unity3d.com/ScriptReference/Collider.OnTriggerEnter.html
---
+++ Scripts/MainMaskPuzzle.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainMaskPuzzle : MonoBehaviour
+{
+    // Start is called before the first frame update
+    // number of mask int
+    void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.tag == "p1")
+        {
+            Debug.Log("right object :P");
+            collision.gameObject.GetComponent<pickObjects>().rb.isKinematic = true;
+            //number of mask ++
+        }
+        else
+        {
+            Debug.Log("wrong one!! ");
+
+        };
+       
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // when masknumber == 4 or whatever then leave the room 
+
+    }
+}
